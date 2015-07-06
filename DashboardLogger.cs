cf94ed8be744ab90c7d80363a9fe07ee6b7da27e
ZurIdo <zur.ido@gmail.com>
2015-07-06 13:42:03 +0200

    test

---
+++ DashboardLogger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public static class DashboardLogger
+{
+
+    public static void writeToLogFile(string logMessage)
+    {
+        string strLogMessage = string.Empty;
+        string strLogFile = System.Configuration.ConfigurationManager.AppSettings["logFilePath"].ToString();
+        StreamWriter swLog;
+
+        strLogMessage = string.Format("{0}: {1}", DateTime.Now, logMessage);
+
+        if (!File.Exists(strLogFile))
+        {
+            swLog = new StreamWriter(strLogFile);
+        }
+        else
+        {
+            swLog = File.AppendText(strLogFile);
+        }
+
+        swLog.WriteLine(strLogMessage);
+        swLog.WriteLine();
+
+        swLog.Close();
+    }
+}
